e6ca0480e2c24ed85806823073853b555db82819
Zach Flynn <zlflynn@gmail.com>
2018-11-09 21:13:32 -0500

    Updated bal version

--- bal.c
+++ bal.c
@@ -17,7 +17,7 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#define BAL_VERSION "0.1.0"
+#define BAL_VERSION "0.1.1"
 
 #include <stdio.h>
 #include <stdlib.h>
