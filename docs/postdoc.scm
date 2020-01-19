8e8be961a55015a8c63a0150dcf829b9f34c4d1e
Zach Flynn <zlflynn@gmail.com>
2020-01-18 23:14:34 -0500

    Gave a link to manual

--- docs/postdoc.scm
+++ docs/postdoc.scm
@@ -12,6 +12,7 @@
                      'pre 1 2 'post))
 
 (let ((output-port (open-file "../README" "w")))
+  (display "To see a full manual for Leas in a tutorial format, see: zflynn.com/leas/index.html\n" output-port)
   (display README-text output-port)
   (close-port output-port))
 
